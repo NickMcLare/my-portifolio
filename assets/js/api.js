747079da3c665bfa11cecf19e0c628a064c271c1
NickMcLare <nicholasousa0807@gmail.com>
2026-02-23 20:41:49 -0300

    Acrescentando algumas coisas que faltavam.

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
  async function fetchProfileData () {
-    const url = ''
+    const url = 'https://raw.githubusercontent.com/NickMcLare/my-portifolio/refs/heads/main/assets/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
